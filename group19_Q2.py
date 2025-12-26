a3fc57663655900b4c99c3445022d00c7948c1dd
annapeach521 <abt7521@gmail.com>
2025-12-26 19:23:07 +0800

    Create group19_Q2.py

---
+++ group19_Q2.py
@@ -0,0 +1,16 @@
+!sudo apt update
+!sudo apt install tesseract-ocr tesseract-ocr-chi-tra
+
+!pip install pytesseract
+
+import pytesseract
+from PIL import Image
+import cv2
+
+# 開啟圖片
+image = Image.open("group_name.png")
+text = pytesseract.image_to_string(image, lang='chi_tra', config='--psm 10')
+
+print("OCR 辨識結果:")
+print("-" * 40)
+print(text)
